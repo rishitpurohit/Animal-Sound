c21ecf9b833daaa182456da218ca49a3e57e2d86
rishitpurohit <94513622+rishitpurohit@users.noreply.github.com>
2022-04-15 20:54:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 //https://teachablemachine.withgoogle.com/models/hf82f1xiG/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/hf82f1xiG/",modelReady);
+
+}
+
+function modelReady(){
+   classifier.classify(gotResults); 
+}
+
+function gotResults(error,results){
+console.log("Got Results");
+}
